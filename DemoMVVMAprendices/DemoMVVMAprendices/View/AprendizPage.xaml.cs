14114dffd9ed670a34e800cd3ac48a28e5a38946
unknown <Aprendiz Sena@TUNDFPCGE7F369.FormacionSena.RED>
2018-05-23 21:13:54 -0500

    Detalle Aprendiz

--- DemoMVVMAprendices/DemoMVVMAprendices/View/AprendizPage.xaml.cs
+++ DemoMVVMAprendices/DemoMVVMAprendices/View/AprendizPage.xaml.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
+ 
 namespace DemoMVVMAprendices.View
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using ViewModel;
+    using Model;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AprendizPage : ContentPage
 	{
 		public AprendizPage ()
 		{
 			InitializeComponent ();
+            this.BindingContext = new AprendizViewModel();
+            lstName.ItemSelected += LstName_ItemSelected; ;
 		}
-	}
+
+        private void LstName_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var aprendizSeleccionado = (Aprendiz)e.SelectedItem;
+            if (aprendizSeleccionado == null)
+            {
+                return;
+            }
+            Navigation.PushAsync(new DetalleAprendiz(aprendizSeleccionado));
+            lstName.SelectedItem = null;
+        }
+    }
 }
